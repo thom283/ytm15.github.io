9c197aae9a22ab8cd273ea3be3d4facb85ffd6f4
thom283 <137968912+thom283@users.noreply.github.com>
2025-04-11 12:15:08 -0700

    Delete app directory

--- app/watchpage.js
+++
@@ -1,9 +0,0 @@
-var spinnerParent = document.querySelector("#app");
-spinner();
-var spinner = document.querySelector(".spinner-container.full-height");
-spinner.setAttribute("hidden", "");
-
-/* const headerBar = document.querySelector("ytm15-header-bar"); */
-if (headerBar) {
-headerBar.setAttribute('hidden', '');
-}
